c0738725550c0e5886e086db02b5e36d762ff126
algor1 <alexeev.igor.v@gmail.com>
2019-08-22 16:16:44 +0300

    class Complex added

---
+++ CourseDz/dz/Complex.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dz
+{
+    public class Complex
+    {
+        public double X { get; }
+        public double I { get; }
+
+        public Complex(double x,double i)
+        {
+            X = x;
+            I = i;
+        }
+
+        public Complex Plus(Complex a)
+        {
+            return new Complex(a.X + X, a.I + I);
+        }
+        public Complex Substruct(Complex a)
+        {
+            return new Complex(a.X -X, a.I - I);
+        }
+        public override string ToString()
+        {
+            if (I >= 0)
+            {
+                return $"{X}+{I}i";
+            }
+            else
+            {
+                return $"{X}-{I*(-1)}i";
+            }
+        }
+    }
+}
--- CourseDz/dz/Program.cs
+++ CourseDz/dz/Program.cs
@@ -21,8 +21,14 @@ namespace dz
 
             //Dz4 d4 = new Dz4();
             //d4.RomanNumeralsParser();
-            Console.WriteLine(Calculator.HeronTriangleArea(3, 3, 3));
-            Console.WriteLine(Calculator.HeronTriangleArea(3, 3, 3d));
+            //Console.WriteLine(Calculator.HeronTriangleArea(3, 3, 3));
+            //Console.WriteLine(Calculator.HeronTriangleArea(3, 3, 3d));
+            Complex c1 = new Complex(1, 1);
+            Complex c2 = new Complex(1, 2);
+
+            Complex result = c1.Plus(c2);
+            Console.WriteLine(result);
+            Console.WriteLine(c1.Substruct(c2));
             Console.ReadKey();
         }
     }
