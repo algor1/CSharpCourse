c410116c41b3a2de18b3d8e121abeac6d2db6f0e
algor1 <alexeev.igor.v@gmail.com>
2019-08-23 15:52:56 +0300

    implement IEnumerable interfase to MyStack class

--- CourseDz/dz/MyStack.cs
+++ CourseDz/dz/MyStack.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace dz
 {
-    public class MyStack<T>
+    public class MyStack<T>:IEnumerable<T>
     {
         private T[] items;
         private int counter;
@@ -43,6 +44,49 @@ namespace dz
                 throw new ArgumentOutOfRangeException("Stack is empty, nothing to peek");
             }
             return items[counter];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new StackIenumerator<T>(items,counter);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+    public class StackIenumerator<T> : IEnumerator<T>
+    {
+        public T Current { get
+            {
+                return array[counter];
+            } }
+        private T[] array;
+        private int counter;
+        
+        public StackIenumerator(T[] array,int counter)
+        {
+            this.array = array;
+            this.counter = counter;
+
+        }
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            counter--;
+            return counter>=0;
+        }
+
+        public void Reset()
+        {
+            counter = array.Length - 1;
         }
     }
 }
--- CourseDz/dz/Program.cs
+++ CourseDz/dz/Program.cs
@@ -32,9 +32,13 @@ namespace dz
 
             MyStack<int> ms = new MyStack<int>();
 
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i < 10; i++)
             {
                 ms.Push(i);
+            }
+            foreach (var item in ms)
+            {
+                Console.WriteLine(item);
             }
             
            
